e7e465a286b6f955720ff50e6cd1d4c4a461db95
Fernando Ferreira <fernandodof@gmail.com>
2020-08-17 20:52:01 +0200

    add content to product details screen

--- screens/shop/product-detail-screen/ProductDetailScreen.tsx
+++ screens/shop/product-detail-screen/ProductDetailScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View, Text, Image, Button, StyleSheet, ScrollView } from 'react-native';
+import { Text, Image, Button, StyleSheet, ScrollView, View } from 'react-native';
 
 import { ProductDetailItemScreenProps } from './procuctDetail.model';
 import { useSelector } from 'react-redux';
-import { RootState } from '../../../store';
 import { NavigationStackScreenComponent } from 'react-navigation-stack';
+
+import { RootState } from '../../../store';
+import Colors from '../../../constants/Colors';
 
 const ProductDetailScreen: NavigationStackScreenComponent = (props: ProductDetailItemScreenProps) => {
 	const productId = props.navigation.getParam('productId');
@@ -12,9 +14,14 @@ const ProductDetailScreen: NavigationStackScreenComponent = (props: ProductDetai
 	const selectedProduct = useSelector((state: RootState) => state.products.availableProducts.find(product => product.id === productId));
 
 	return (
-		<View>
-			<Text>{selectedProduct?.title}</Text>
-		</View>
+		<ScrollView>
+			<Image style={styles.image} source={{ uri: selectedProduct?.imageUrl }}></Image>
+			<View style={styles.actions}>
+				<Button color={Colors.primary} title="Add to cart" onPress={() => { }}></Button>
+			</View>
+			<Text style={styles.price}>${selectedProduct?.price.toFixed(2)}</Text>
+			<Text style={styles.description}>{selectedProduct?.description}</Text>
+		</ScrollView>
 	);
 }
 
@@ -25,7 +32,25 @@ ProductDetailScreen.navigationOptions = navData => {
 };
 
 const styles = StyleSheet.create({
-
+	image: {
+		width: '100%',
+		height: 300
+	},
+	actions: {
+		marginVertical: 10,
+		alignItems: 'center'
+	},
+	price: {
+		fontSize: 22,
+		color: '#800',
+		marginVertical: 20,
+		textAlign: 'center'
+	},
+	description: {
+		fontSize: 16,
+		textAlign: 'center',
+		marginHorizontal: 20
+	}
 });
 
 export default ProductDetailScreen;
